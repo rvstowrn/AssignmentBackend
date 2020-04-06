86020a10c1d4434511cc2ef0fab3b4357a49d92f
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2020-04-06 23:42:43 +0530

    Fees backend v2

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use("/api/admin", require("./routes/api/Admin/adminTaskHandler"));
 app.use("/api/admin", require("./routes/api/Admin/adminTimetablesHandler"));
 app.use("/api/admin", require("./routes/api/Admin/adminUserHandler"));
 app.use("/api/admin", require("./routes/api/Admin/adminSubjectHandler"));
+app.use("/api/admin", require("./routes/api/Admin/adminFeesHandler"));
 
 app.use("/api/students", require("./routes/api/Users/Student"));
 app.use("/api/teachers", require("./routes/api/Users/Teacher"));
